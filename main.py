5cde59851d6c199b5e4fe89f6c3220d27dfda113
Eznix <eznix@hotmail.com>
2024-08-20 18:53:55 +0100

    added delay to shots

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids,updatable,drawable)
     AsteroidField.containers = (updatable)
-    shots.containers = (shots,updatable,drawable)
+    Shot.containers = (shots,updatable,drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidField = AsteroidField()
